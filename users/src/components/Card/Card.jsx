9a61e2e3594e41cb44de4d905f501b3349b413b2
Mohana-Sowdesh <mohanasowdesh.ramadoss@cdw.com>
2024-02-06 15:44:03 +0530

    added user image, font awsome icon,
    Updated styles

--- users/src/components/Card/Card.jsx
+++ users/src/components/Card/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Tag from '../Tag/Tag';
-import styles from '../Card/Card.module.scss';
+import styles from '../Card/Card.module.scss'; 
 
 function Card(props) {
     const interestsList = props.user.interests.map(interest => <Tag interest={interest}/>)
     return (
         <div className={styles['card-container']}>
-            <div>
-                <img src={props.user.imgUrl} alt="photo of user"/>
+            <div className={styles['user-photo']}>
+                <img src="/images/user.jpg" alt="photo of user"/>
             </div>
             <div className={styles['user-details']}>
-                <div>{props.user.name}</div>
-                <div>{props.user.place}</div>
+                <div className={styles['user-name']}>{props.user.name}</div>
+                <div className={styles['place']}>{props.user.place}</div>
                 <div className={styles['interests-container']}>{interestsList}</div>
             </div>
         </div>
--- users/src/components/Searchbox/Searchbox.jsx
+++ users/src/components/Searchbox/Searchbox.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../Searchbox/Searchbox.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Searchbox() {
     return (
         <div className={styles['search-box']}>
-            <FontAwesomeIcon icon="fa-light fa-magnifying-glass" className={styles['search-icon']} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} className={styles['search-icon']}/>
             <input type="text" placeholder='Search users' className={styles['search-input']}></input>
         </div>
     );
